4a34886ac4a8a9b387bd824df1cbe05ad46e33b5
Maedeh Javadi <61285770+maejav@users.noreply.github.com>
2025-10-07 07:37:29 -0700

    Update PolyNomialWithOop.cpp

--- CPlusPlusBasicCode/PolyNomialWithOop/PolyNomialWithOop.cpp
+++ CPlusPlusBasicCode/PolyNomialWithOop/PolyNomialWithOop.cpp
@@ -1,237 +1,209 @@
-#include <cstdlib>
 #include <iostream>
-#include <windows.h>
+#include <cstdlib>
 #include <conio.h>
 using namespace std;
-void error(const char *msg)
-{
-     cout<<msg<<"\n";
-     getch();
-     exit (0);
+
+void error(const char* msg) {
+    cout << msg << "\n";
+    getch();
+    exit(1);
 }
 
-    class poly {
-          private:
-           double *coe1;
-           int *exp1;
-           int n;
-        public:
-               poly(){coe1=0;  exp1=0;   n=0; /*cout<<"constroctor!\n";*/}
-               
-               poly(int m){ coe1=new double[n=1];  exp1=new int[n=1];  coe1[0]=exp1[0]=m; /* cout<<"constroctor!!!!!\n";*/   }
-               
-              ~poly(){delete []exp1; delete []coe1;  exp1=0;coe1=0; n=0; /*cout<<"destroctor!\n";*/ }
-    void input_poly(){
-         
-         
-         
-         cout<<"enter number of terms\n";
-         cin>>n;
-         if(n<0)
-         error("n<0 ::::::::");
-         coe1= new double [n];  exp1=new int [n];
-         if(!coe1||!exp1){
-                          error("memory allocation error!!!!");
-                          }//end of if
-          for(int i=0;i<n;i++){
-                  cout<<"enter coe1["<<i<<"]:";  cin>>coe1[i];
-                  cout<<"enter exp1["<<i<<"]:";  cin>>exp1[i];
-                  
-                  }
-                  }
-    void print_poly (){
-                                                       
-     for(int i=0;i<n;i++)
-     {
-             if(coe1[i]!=0)
-             cout<<coe1[i];
-             else if (i!=0&&coe1[i]==0)
-             cout<<"+";
-             if(exp1[i]!=0&&coe1[i]!=0)
-             {
-                          cout<<"x";
-                          if(exp1[i]!=1)
-                          cout<<"^"<<exp1[i];
-                          }
-                          if(i!=n-1)
-                          {
-                          if (coe1[i+1]>0&&coe1[i]!=0)
-                          {
-                                       cout<<"+";
-                                       }
-                                       }
-                                       }//end of for
-                                       cout<<"\n";
-                                       }//end of display
-                                       
-                                       
-                                       
-       poly & operator=(const poly &t){
-            
-            delete []coe1; delete []exp1;  n=0;
-            coe1=new double [n=t.n];  exp1=new int [n=t.n];
-            for(int i=0;i<n;i++){
-                    coe1[i]=t.coe1[i];
-                    exp1[i]=t.exp1[i];
-                    }
-                    return *this;
-                    }
-                    
-                    
-                    poly(const poly & t){// sazande kopy baraye jelogiri az kopy haye nasahih//
-                               cout<<"copy";
-                              delete []exp1; delete []coe1; n=0;
-                              coe1=new double [n=t.n];  exp1=new int [n=t.n];
-                              for(int i=0;i<t.n;i++){
-                                      coe1[i]=t.coe1[i];
-                                      exp1[i]=t.exp1[i];
-                                      }
-                                      }
-                                      
-                                      
-            friend poly  operator+(const poly &f,const poly &t);//tabe doost!!!!!!
-            friend poly operator*(poly &p1,poly &p2);
-                                                                                                             
-                                                                                        
-                                  };  
-                                  poly operator+(const poly &f,const poly &t){
-                                       poly res;//komaki
-                                       res.n= f.n + t.n;
-                                       res.coe1=new double [res.n];  res.exp1=new int [res.n];
-                                        
-                                        int r=0;//shomarande
-                                       
-                                       for (int i=0;i<f.n;i++){
-                                           for(int j=0;j<t.n ; j++){
-                                                   
-                                           if(f.exp1[i]==t.exp1[j]){//shart barabari tavan ha baraye amal jam
-                                           res.coe1[r]=f.coe1[i]+t.coe1[j];
-                                           res.exp1[r]=f.exp1[i];
-                                           r++;
-                                           }//end of if
-                                           }
-                                           }
-                                           
-                                          int  m=r;
-                                          int val=1;
-                                          
-                                           for (int i=0;i<f.n;i++){
-                                               for (int j=0;j<m;j++){
-                                               if(f.exp1[i]==res.exp1[j])
-                                               {
-                                                                        val=0; //agar tvan mojud bud
-                                                                         break;
-                                                                         }
-                                                                         }
-                                                                         if(val==1)
-                                                                         {
-                                                                                   
-                                                                         res.coe1[r]=f.coe1[i];//agar val sefr nashod
-                                                                         res.exp1[r]=f.exp1[i];
-                                                                         r++;
-                                                                        
-                                                                         }
-                                                                         else
-                                                                         val=1;
-                                                                         }//////
-                                                                         
-                                                                         m=r;
-                                                                         
-                                           for (int i=0;i<t.n;i++){
-                                               for (int j=0;j<m;j++){
-                                               if(t.exp1[i]==res.exp1[j])
-                                               {
-                                                                        val=0; 
-                                                                         break;
-                                                                         }
-                                                                         }
-                                                                         if(val==1)
-                                                                         {
-                                                                                   
-                                                                         res.coe1[r]=t.coe1[i];
-                                                                         res.exp1[r]=t.exp1[i];
-                                                                         r++;
-                                                                         }
-                                                                         else
-                                                                         val=1;
-                                                                         }////
-                                                                         res.n=r;
-                                                                         return res;
-                                                                         }//end of tabe
-                                                                         
-                                                                         
-                                                                         
-                            poly operator*(poly &p1,poly &p2)
-                           {
-                                 poly p3;
-                                 p3.n=p1.n*p2.n;
-                                 p3.coe1=new double [p3.n];  p3.exp1=new int [p3.n];
-                                 if(p3.coe1&&p3.exp1){
-                                 int f=0;
-                                for (int i=0;i<p1.n;i++)
-                                {
-                                    for (int j=0;j<p2.n;j++)
-                                    {
-                                        p3.coe1[f]=p1.coe1[i]*p2.coe1[j];
-                                        p3.exp1[f]=p1.exp1[i]+p2.exp1[j];
-                                        f++;
-                                        }
-                                        }
-                                        p3.n=f--;
-                                        for(int i=1;i<f;i++)
-                                        {
-                                                for(int j=i+1;j<f;j++)
-                                                {
-                                                        if(p3.exp1[i]==p3.exp1[j])//agar tavan ha barabar bud ba ham jam shavand
-                                                        {
-                                                                            p3.n--;
-                                                                            p3.coe1[i]=p3.coe1[i]+p3.coe1[j];
-                                                                            for(int k=j;k<f;k++)//dar in halghe an khanei ke jam shod hazf mishavad
-                                                                            {
-                                                                                    p3.coe1[k]=p3.coe1[k+1];
-                                                                                    p3.exp1[k]=p3.exp1[k+1];
-                                                                            
-                                                                                    }
-                                                                                    
-                                                                                    }//end of if
-                                                                                                                                                    
-                                                                             }  
-                                                                             }
-                                                                             }
-                                                                             else
-                                                                             error("memory error!");
-                                                                             return p3;
-                                                                                                                                                  
-                                                                                                                                              
-                                                                                                                                                    
-                                                                                                 }//end of tabe
-                                           
-                                           
-                                        
-                                    
-                         
-int main(int argc, char *argv[])
-{
-    poly p1;
-    p1=3;
-    poly p2, p3;
-    p2.input_poly();
-    p3.input_poly();
-    cout<<"p1:\n";
-    p1.print_poly();
-    cout<<"p2:\n";
-    p2.print_poly();
-    cout<<"p3:\n";
-    p3.print_poly();
-    p1=p2+p3;
-    cout<<"p1=p2+p3:\n";
-    p1.print_poly();
-    p1=p2*p3;
-    cout<<"p1=p2*p3:\n";
-    p1.print_poly();
-            
-                       
-    system("PAUSE");
+class Poly {
+private:
+    double* coe;
+    int* exp;
+    int n;
 
-    return EXIT_SUCCESS;
+public:
+    // Default constructor
+    Poly() : coe(nullptr), exp(nullptr), n(0) {}
+
+    // Parameterized constructor
+    Poly(int value) {
+        n = 1;
+        coe = new double[n];
+        exp = new int[n];
+        coe[0] = value;
+        exp[0] = value;
+    }
+
+    // Copy constructor
+    Poly(const Poly& other) {
+        n = other.n;
+        coe = new double[n];
+        exp = new int[n];
+        for (int i = 0; i < n; ++i) {
+            coe[i] = other.coe[i];
+            exp[i] = other.exp[i];
+        }
+    }
+
+    // Assignment operator
+    Poly& operator=(const Poly& other) {
+        if (this != &other) {
+            delete[] coe;
+            delete[] exp;
+            n = other.n;
+            coe = new double[n];
+            exp = new int[n];
+            for (int i = 0; i < n; ++i) {
+                coe[i] = other.coe[i];
+                exp[i] = other.exp[i];
+            }
+        }
+        return *this;
+    }
+
+    // Destructor
+    ~Poly() {
+        delete[] coe;
+        delete[] exp;
+        coe = nullptr;
+        exp = nullptr;
+        n = 0;
+    }
+
+    void input() {
+        cout << "Enter number of terms: ";
+        cin >> n;
+        if (n <= 0) error("Invalid number of terms");
+
+        coe = new double[n];
+        exp = new int[n];
+        if (!coe || !exp) error("Memory allocation failed");
+
+        for (int i = 0; i < n; ++i) {
+            cout << "Enter coefficient[" << i << "]: ";
+            cin >> coe[i];
+            cout << "Enter exponent[" << i << "]: ";
+            cin >> exp[i];
+        }
+    }
+
+    void display() const {
+        for (int i = 0; i < n; ++i) {
+            if (coe[i] == 0) continue;
+
+            if (i > 0 && coe[i] > 0) cout << "+";
+            cout << coe[i];
+
+            if (exp[i] != 0) {
+                cout << "x";
+                if (exp[i] != 1) cout << "^" << exp[i];
+            }
+        }
+        cout << "\n";
+    }
+
+    // Friend function for addition
+    friend Poly operator+(const Poly& a, const Poly& b) {
+        Poly result;
+        result.n = a.n + b.n;
+        result.coe = new double[result.n];
+        result.exp = new int[result.n];
+
+        int r = 0;
+        // Add matching exponents
+        for (int i = 0; i < a.n; ++i) {
+            for (int j = 0; j < b.n; ++j) {
+                if (a.exp[i] == b.exp[j]) {
+                    result.coe[r] = a.coe[i] + b.coe[j];
+                    result.exp[r] = a.exp[i];
+                    ++r;
+                }
+            }
+        }
+
+        // Add unmatched terms from a
+        for (int i = 0; i < a.n; ++i) {
+            bool found = false;
+            for (int j = 0; j < r; ++j) {
+                if (a.exp[i] == result.exp[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                result.coe[r] = a.coe[i];
+                result.exp[r] = a.exp[i];
+                ++r;
+            }
+        }
+
+        // Add unmatched terms from b
+        for (int i = 0; i < b.n; ++i) {
+            bool found = false;
+            for (int j = 0; j < r; ++j) {
+                if (b.exp[i] == result.exp[j]) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                result.coe[r] = b.coe[i];
+                result.exp[r] = b.exp[i];
+                ++r;
+            }
+        }
+
+        result.n = r;
+        return result;
+    }
+
+    // Friend function for multiplication
+    friend Poly operator*(const Poly& a, const Poly& b) {
+        Poly result;
+        result.n = a.n * b.n;
+        result.coe = new double[result.n];
+        result.exp = new int[result.n];
+
+        int r = 0;
+        for (int i = 0; i < a.n; ++i) {
+            for (int j = 0; j < b.n; ++j) {
+                result.coe[r] = a.coe[i] * b.coe[j];
+                result.exp[r] = a.exp[i] + b.exp[j];
+                ++r;
+            }
+        }
+
+        // Merge like terms
+        for (int i = 0; i < r; ++i) {
+            for (int j = i + 1; j < r; ++j) {
+                if (result.exp[i] == result.exp[j]) {
+                    result.coe[i] += result.coe[j];
+                    for (int k = j; k < r - 1; ++k) {
+                        result.coe[k] = result.coe[k + 1];
+                        result.exp[k] = result.exp[k + 1];
+                    }
+                    --r;
+                    --j;
+                }
+            }
+        }
+
+        result.n = r;
+        return result;
+    }
+};
+
+int main() {
+    Poly p1(3); // single-term polynomial
+    Poly p2, p3;
+
+    p2.input();
+    p3.input();
+
+    cout << "p1:\n"; p1.display();
+    cout << "p2:\n"; p2.display();
+    cout << "p3:\n"; p3.display();
+
+    p1 = p2 + p3;
+    cout << "p1 = p2 + p3:\n"; p1.display();
+
+    p1 = p2 * p3;
+    cout << "p1 = p2 * p3:\n"; p1.display();
+
+    system("PAUSE");
+    return 0;
 }
