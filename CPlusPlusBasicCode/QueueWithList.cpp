f3ae9541dbb2d5a03634b5cc538a6b3bc0958950
Maedeh Javadi <61285770+maejav@users.noreply.github.com>
2025-10-08 04:54:58 -0700

    Update QueueWithList.cpp

--- CPlusPlusBasicCode/QueueWithList.cpp
+++ CPlusPlusBasicCode/QueueWithList.cpp
@@ -1,193 +1,89 @@
 #include <iostream>
-#include <conio.h>
-#include<cstdlib>
-#include<Windows.h>
-#include<string>
+#include <string>
 using namespace std;
-class node {
-private:
-int info;
-node  *next;
-public:
-	node(){
-		next=NULL;
-	}
-	//~node(){
-		//delete next;           
-	//}*/
-	friend class saf;
 
+class Node {
+public:
+    int info;
+    Node* next;
+
+    Node(int val) : info(val), next(nullptr) {}
 };
 
-
-class saf  {
-public:
-	saf(string nam){
-		name=nam;
-		first=NULL;
-	last=NULL;
-	}
-	//~saf(){
-	//delete first;
-	//}
-	void insert (int t){
-		node *newptr;
-		newptr=new node;
-		if(first==NULL&&last==NULL){
-		first=last=newptr;
-		newptr->info=t;
-		newptr->next=NULL;
-		}
-		else{
-		newptr->info=t;
-		newptr->next=NULL;
-		last->next=newptr;
-		last=newptr;
-		}
-
-	}//end of insert
-	void del( void){
-		if(empty()){
-		node *ptr ;
-		ptr=first;
-		if(first->next==NULL)
-			last=NULL;
-		first=first->next;
-		//delete ptr;
-		}
-		else
-			cout<<"queue is empty for:"<<name<<"\n";
-
-	}
-	bool empty(){
-		if(first==last&&last==NULL){
-			cout<<"queue is empty for:"<<name<<"\n";
-			return  false;
-		}
-		
-			return true;
-	}/*
-int number  (void){
-	if(empty()){                                         ******************************************
-	int num =(last->info)-(first ->info)+1;
-	cout<<"for:"<<name<<"\n";                                                          *****************************
-	return num;
-	}else
-		return 0;
-}///end of number
-/////ORRRRRRRRRRRRRRRR*/
-
-int number (void){
-	if(empty()){
-		int sh=1;
-		for(node*ptr=first;ptr->next!=NULL;ptr=ptr->next)
-			sh++;
-		cout<<"for:"<<name<<"\n";
-		return sh;
-	}else
-		return 0;
-}
-void print (void){
-	for(node *ptr=first;ptr!=NULL;ptr=ptr->next)
-		cout<<"queue:"<<ptr->info<<" ";
-	cout<<"\n";
-}
-
-
+class Queue {
 private:
-	node *first ;
-	node *last;
-	string name;
+    Node* first;
+    Node* last;
+    string name;
 
+public:
+    Queue(string queueName) : first(nullptr), last(nullptr), name(queueName) {}
 
+    void insert(int val) {
+        Node* newNode = new Node(val);
+        if (!first) {
+            first = last = newNode;
+        } else {
+            last->next = newNode;
+            last = newNode;
+        }
+    }
 
+    void remove() {
+        if (isEmpty()) {
+            cout << "Queue is empty for: " << name << "\n";
+            return;
+        }
+        Node* temp = first;
+        first = first->next;
+        if (!first) last = nullptr;
+        delete temp;
+    }
+
+    bool isEmpty() const {
+        return first == nullptr;
+    }
+
+    int count() const {
+        int total = 0;
+        for (Node* ptr = first; ptr != nullptr; ptr = ptr->next)
+            total++;
+        return total;
+    }
+
+    void print() const {
+        cout << "Queue for " << name << ": ";
+        for (Node* ptr = first; ptr != nullptr; ptr = ptr->next)
+            cout << ptr->info << " ";
+        cout << "\n";
+    }
 };
-int main(){
-	int exit=1;
-	int num_m=0;
-	int num_f=0;
-	char ge,pa;
-	char turn='m';
-	saf male("males"),female("females");
-	while(exit==1){
-		cout<<"if yout want to insert in queue press:'e'"<<"\n";
-		cout<<"if you want to turn press:'n'"<<"\n";
-		cout<<"if you want to see numbers of person in queue press:'s'\n";
-		cout<<"if you want to exit press:'k'\n";
-		cin>>pa;
-		switch(pa){
-		case 'e': 
-			char mz;
-				  cout<<"male or female\n";
-				  cin>>mz;
-				  if(mz =='m'){
-					  num_m++;
-					  male.insert(num_m);
-				  }
-				  else if (mz=='f'){
-					  num_f++;
-					  female.insert(num_f);
-				  }
-				  else cout<<"ERORRRRRRRRRRR!!!!!!\N";
-				  break;
-		case 'n':
-		
-			
-			if(turn=='f'){
-				if(male.empty()){
-					male.del();
-					turn='m';
-				}
-				else if(female.empty()){
-						female.del();
-						turn='f';
-					}
-					
-			}
-			 else if(turn=='m'){
-					if(female.empty()){
-						female.del();
-						turn='f';
-					}
-					   else if(male.empty()){
-					    male.del();
-					     turn='m';
-					    }
-			}
-			/* else{
-				 cout<<"both of them are empty!\n";
-				 getch();
-			 }*/
-			
-				
-			
-              break;
-	     	case 's':
-	  		int num1;
-			num1=female.number();
-			cout<<num1<<"\n";
-			int num2;
-			num2=male.number();
-			cout<<num2<<"\n";
-			getch();
-			break;
-			
-		    case 'k':
-				cout<<"End of program!\n";
-				getch();
-			exit=0;
-			break;
-		/*default :
-				cout<<"ERORRRRRRRRRRRRRR!!!!!!!!!\n";
-				break;*/
 
-		}
-		system ("cls");
+int main() {
+    Queue male("males"), female("females");
+    int maleCount = 0, femaleCount = 0;
+    char action, gender, turn = 'm';
+    bool running = true;
 
+    while (running) {
+        cout << "\nMenu:\n";
+        cout << "Insert into queue: 'e'\n";
+        cout << "Serve next person: 'n'\n";
+        cout << "Show queue counts: 's'\n";
+        cout << "Exit: 'x'\n";
+        cin >> action;
 
-		}//end of while
+        switch (action) {
+        case 'e':
+            cout << "Enter gender (m/f): ";
+            cin >> gender;
+            if (gender == 'm') {
+                male.insert(++maleCount);
+            } else if (gender == 'f') {
+                female.insert(++femaleCount);
+            } else {
+                cout << "Invalid gender input.\n";
+            }
+            break;
 
-
-return 0;
-
-}
+        case 'n':
