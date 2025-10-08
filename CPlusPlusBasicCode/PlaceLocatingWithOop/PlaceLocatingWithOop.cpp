8640b471a8f2ed21ed700ac72fba9f3a62e6108d
Maedeh Javadi <61285770+maejav@users.noreply.github.com>
2025-10-08 04:22:58 -0700

    Update PlaceLocatingWithOop.cpp

--- CPlusPlusBasicCode/PlaceLocatingWithOop/PlaceLocatingWithOop.cpp
+++ CPlusPlusBasicCode/PlaceLocatingWithOop/PlaceLocatingWithOop.cpp
@@ -1,39 +1,41 @@
-#include <cstdlib>
 #include <iostream>
-
 using namespace std;
-struct place {
-       int x;
-       int y;
-       void input(){
-            cout<<"enter place of object\n";
-            cout<<"x=";
-            cin>>x;
-            cout<<"y=";
-            cin>>y;
-            }
-            void move (){
-                 int dx,dy;
-                 cout<<"enter dx and dy\n";
-                 cin>>dx>>dy; 
-                 x=x+dx;
-                 y=y+dy;
-                 }
-                 void print (){
-                      cout<<"("<<x<<","<<y<<")"<<"\n";
-                      }
-                      };
-int main(int argc, char *argv[])
-{
-    
-    struct place p1;
+
+struct Place {
+    int x;
+    int y;
+
+    void input() {
+        cout << "Enter initial position of the object:\n";
+        cout << "x = ";
+        cin >> x;
+        cout << "y = ";
+        cin >> y;
+    }
+
+    void move() {
+        int dx, dy;
+        cout << "Enter movement vector (dx and dy):\n";
+        cin >> dx >> dy;
+        x += dx;
+        y += dy;
+    }
+
+    void print() const {
+        cout << "Current position: (" << x << ", " << y << ")\n";
+    }
+};
+
+int main() {
+    Place p1;
+
     p1.input();
-    cout<<"place of object before movement:";
+    cout << "\nBefore movement:\n";
     p1.print();
+
     p1.move();
-    cout<<"place of object after movement:";
+    cout << "\nAfter movement:\n";
     p1.print();
-     
-    system("PAUSE");
-    return EXIT_SUCCESS;
+
+    return 0;
 }
